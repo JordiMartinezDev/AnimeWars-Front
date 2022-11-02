f46f2988d98a8106d82220fc989018c82d4e5e7a
Jordi Martinez <jordiupc87@gmail.com>
2022-11-02 10:18:35 +0100

    branches

--- src/components/IsAnon/IsAnon.jsx
+++ src/components/IsAnon/IsAnon.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "../../context/auth.context";
 import { Navigate } from "react-router-dom";
 import Loading from "../Loading/Loading";
-
+//to commit
 function IsAnon({ children }) {
   const { isLoggedIn, isLoading } = useContext(AuthContext);
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import "./HomePage.css";
 function HomePage() {
   return (
     <div>
-      <h1>Home page</h1>
+      <h1>Home pages</h1>
     </div>
   );
 }
