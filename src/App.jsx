4fe3e4a33a9062589c9b10098d3ab87299d18fde
Jordi Martinez <jordiupc87@gmail.com>
2022-11-02 15:24:43 +0100

    Routes&Pages created

--- src/App.jsx
+++ src/App.jsx
@@ -5,44 +5,28 @@ import HomePage from "./pages/HomePage/HomePage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"
 
 import Navbar from "./components/Navbar/Navbar";
 import IsPrivate from "./components/IsPrivate/IsPrivate";
 import IsAnon from "./components/IsAnon/IsAnon";
+import Loading from "./components/Loading/Loading";
+import CreateEpisode from "./pages/CreateEpisode/CreateEpisode";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
+      
       <Routes>
+
         <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={ <IsPrivate> <ProfilePage /> </IsPrivate>} />
+        <Route path="/signup" element={ <IsAnon> <SignupPage /> </IsAnon>}/>
+        <Route path="/login" element={<IsAnon> <LoginPage /> </IsAnon>} />
+        <Route path="*" element={<NotFoundPage></NotFoundPage>} />
+        <Route path="/createEpisode" element={<CreateEpisode></CreateEpisode>}/>
 
-        <Route
-          path="/profile"
-          element={
-            <IsPrivate>
-              <ProfilePage />
-            </IsPrivate>
-          }
-        />
-
-        <Route
-          path="/signup"
-          element={
-            <IsAnon>
-              <SignupPage />
-            </IsAnon>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <IsAnon>
-              <LoginPage />
-            </IsAnon>
-          }
-        />
       </Routes>
     </div>
   );
