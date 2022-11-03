48c3f40c1ffd224c2bb18ccfb54b78f91d3ec8a7
Jordi Martinez <jordiupc87@gmail.com>
2022-11-03 15:40:57 +0100

    Working on API monday noon

--- src/services/animeAPI.service.js
+++ src/services/animeAPI.service.js
@@ -1,24 +1,42 @@
 import axios from "axios";
 
-// const apiUrl = "https://apartmentsback.herokuapp.com/api/apartments";
-const apiUrl = "http://localhost:5005/api/apartments";
+const apiUrl = "http://localhost:5005/api";
 
 class AnimeApiService {
   getAnimes() {
-    return axios.get(apiUrl);
+    return axios.get(apiUrl + "/animes");
   }
   getAnime(animeId) {
-    return axios.get(apiUrl + "/" + animeId);
+    return axios.get(apiUrl + "/animes" + animeId);
   }
   deleteAnime(animeId) {
-    return axios.delete(apiUrl + "/" + animeId);
+    return axios.delete(apiUrl + "/animes" + animeId);
   }
   addAnime(anime) {
     // return axios.post(apiUrl, {img: apartment.img, title: apartment.title, pricePerDay: apartment.pricePerDay});
-    return axios.post(apiUrl, anime);
+    return axios.post(apiUrl + "/animes", anime);
   }
   editAnime(anime) {
-    return axios.put(apiUrl + "/" + anime._id, anime);
+    return axios.put(apiUrl + "/animes" + anime._id, anime);
+  }
+
+  // ------ Episodes -------
+
+  getEpisodes() {
+    return axios.get(apiUrl + "/episodes");
+  }
+  getEpisode(episodeId) {
+    return axios.get(apiUrl + "/episodes" + episodeId);
+  }
+  deleteEpisode(episodeId) {
+    return axios.delete(apiUrl + "/episodes" + episodeId);
+  }
+  addEpisode(episode) {
+    // return axios.post(apiUrl, {img: apartment.img, title: apartment.title, pricePerDay: apartment.pricePerDay});
+    return axios.post(apiUrl + "/episodes", episode);
+  }
+  editEpisode(episode) {
+    return axios.put(apiUrl + "/episodes" + episode._id, episode);
   }
 }
 
