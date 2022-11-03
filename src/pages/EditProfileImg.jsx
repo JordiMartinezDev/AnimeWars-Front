170d295470cbb5d7957040d309f914018fb7f988
Josep Turro <jturrocasanovas@gmail.com>
2022-11-03 15:40:11 +0100

    fent pages

---
+++ src/pages/EditProfileImg.jsx
@@ -0,0 +1,14 @@
+
+
+function EditProfileImg() {
+  return (
+    <div>
+      <h1>Hola desde el component/page EditProfileImg</h1>
+      <p>Aqui posem editar img de perfil i la img del background de perfil</p>
+    </div>
+  );
+}
+
+export default EditProfileImg;
+
+
