be689f38000cc34341f8c413e651125359330007
Jordi Martinez <jordiupc87@gmail.com>
2022-11-08 16:14:15 +0100

    before merge

--- src/components/ShowAnime/ShowAnime.jsx
+++ src/components/ShowAnime/ShowAnime.jsx
@@ -1,18 +1,40 @@
 import {Link} from 'react-router-dom';
 import React from 'react';
 import { useParams } from 'react-router-dom'
+import { useEffect,useState } from 'react';
+import animeAPI from '../../services/animeAPI.service';
 
 
 function ShowAnime() {
-    const {animeId} = useParams()
+    const { animeId } = useParams()
+    const [anime, setAnime] = useState([]);
     // const {anime} = props;
 
+    useEffect( ()=> {
 
+        animeAPI.getAnime(animeId)
+            .then(response => {
+                setAnime(response.data)
+                console.log("Anime in ShowAnime: ",response.data)
+        })
 
+    },[])
+
+    function handleLike() {
+        animeAPI.editAnime(animeId)
+            .then(result => {
+                console.log("Like Handled? -> ", anime.followedUsers )
+        })
+    }
+    function handleUnlike() {
+        
+    }
+   
     return (
         <>
             {console.log(animeId)}
-            {true ? <button> Liked</button>:<button>Not Liked Yet</button>}
+            {true ? <button onClick={handleUnlike}> Liked</button> : <button onClick={handleLike}>Not Liked Yet</button>}
+            
         </>
     );
 }
