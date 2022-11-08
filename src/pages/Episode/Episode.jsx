9c958b682fddf04171f646ac9576400c2139fdb3
Jordi Martinez <jordiupc87@gmail.com>
2022-11-08 17:47:16 +0100

    bugs fixed

--- src/pages/Episode/Episode.jsx
+++ src/pages/Episode/Episode.jsx
@@ -33,7 +33,6 @@ function Episode() {
             width="100%"
             height="500px"
             playing
-            playIcon={<button>Play</button>}
             light="https://i.stack.imgur.com/zw9Iz.png"
             />
         </div>
